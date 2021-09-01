6d86cd4863a3e1d68fe502dc7dd55b6704a6cfd1
Md Shahnawaz Alam <89138005+mdshahnawazalamali@users.noreply.github.com>
2021-09-01 15:34:59 +0530

    Add files via upload

---
+++ List.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function List(){
+    return (
+        <ol>
+            <li>C</li>
+            <li>C++</li>
+            <li>Java</li>
+            <li>Python</li>
+        </ol>
+    );
+}
+export default List;
